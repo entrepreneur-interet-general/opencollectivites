102f0025971d9aa140447c16a31b9ef0f5b7fca5
Ash_Crow <sylvain@ashtree.eu>
2020-11-02 16:49:21 +0100

    [francesubdivisions] Validators for Siren and Insee communes

--- francesubdivisions/models.py
+++ francesubdivisions/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+from stdnum.fr import siren
+from django.utils.translation import gettext_lazy as _
+
+# Validators
+validate_insee_commune = RegexValidator(r"^\d[0-9AB][0-9P]\d\d$")
 
 
+def validate_siren(value):
+    try:
+        siren.validate(value)
+    except (InvalidChecksum, InvalidFormat, InvalidLength):
+        raise ValidationError(
+            _("%(value)s is not an valid siren id"),
+            params={"value": value},
+        )
+
+
+# Models
 class Region(models.Model):
     """
     A French région
