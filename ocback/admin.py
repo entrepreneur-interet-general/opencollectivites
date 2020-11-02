a33c266a8ca3c6fe181692d616b1e855a0610981
Ash_Crow <sylvain@ashtree.eu>
2020-11-02 15:19:42 +0100

    [ocback]Improving the model

--- ocback/admin.py
+++ ocback/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 from ocback import models
 
 admin.site.register(models.Topic)
+admin.site.register(models.Scope)
+admin.site.register(models.Editor)
+admin.site.register(models.Platform)
--- ocback/models.py
+++ ocback/models.py
@@ -16,3 +16,54 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Scope(models.Model):
+    """
+    (fr: Portée)
+    The reach of a site or document (national, regional, etc.)
+    """
+
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["created"]
+
+    def __str__(self):
+        return self.name
+
+
+class Editor(models.Model):
+    """
+    (fr: Éditeur)
+    The editor of a site or document
+    """
+
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to="logos")
+
+    class Meta:
+        ordering = ["created"]
+
+    def __str__(self):
+        return self.name
+
+
+class Platform(models.Model):
+    """
+    (fr:Plateforme)
+    A website.
+    """
+
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    editor = models.ForeignKey("Editor", on_delete=models.SET_NULL, null=True)
+    rssfeed = models.ForeignKey("feeds.Source", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ["created"]
+
+    def __str__(self):
+        return self.name
