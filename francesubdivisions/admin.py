7e76027cfcdc41fa751ac611d4fd1a623a225b6d
Ash_Crow <sylvain@ashtree.eu>
2020-11-03 16:44:28 +0100

    [francesubdivisions] Admin search for communes

--- francesubdivisions/admin.py
+++ francesubdivisions/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from francesubdivisions import models
 
+
+class CommuneAdmin(admin.ModelAdmin):
+    search_fields = ("name", "insee", "siren")
+
+
 admin.site.register(models.Region)
 admin.site.register(models.Departement)
 admin.site.register(models.EpciType)
 admin.site.register(models.Epci)
-admin.site.register(models.Commune)
+admin.site.register(models.Commune, CommuneAdmin)
