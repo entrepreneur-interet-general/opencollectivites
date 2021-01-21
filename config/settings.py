5aa4a37f4095794e3a436be33c88e241e69742c3
Ash_Crow <sylvain@ashtree.eu>
2021-01-21 10:17:15 +0100

    [Config] Change default language to French

--- config/settings.py
+++ config/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fr-fr"
 
 TIME_ZONE = "UTC"
 
