5390c26b2573deadfd69ecb592f48f0536696f2a
Ash_Crow <sylvain@ashtree.eu>
2021-02-05 18:28:59 +0100

    Improved 'homepage'

--- core/views.py
+++ core/views.py
@@ -2,4 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Bienvenue sur le projet Open Collectivités!")
+    return HttpResponse(
+        """
+        <h1>Bienvenue sur Open Collectivités!</h1>
+        <p>La documentation de l’API se trouve sur la page <a href="api/docs">/api/docs</a> 
+        """
+    )
