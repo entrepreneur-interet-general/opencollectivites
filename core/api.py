bc7967b83ad83ef0f2edd7d198072b0d5f457a6c
Ash_Crow <sylvain@ashtree.eu>
2021-02-18 15:50:17 +0100

    [API] Api definition for scopes, topic, documents and sources

--- core/api.py
+++ core/api.py
@@ -1,10 +1,22 @@
 from ninja import Router
 from typing import List
 
-from core.models import Topic, Document
-from core.schemas import TopicSchema, DocumentSchema
+from core.models import Scope, Topic, Document
+from core.schemas import ScopeSchema, TopicSchema, FilterSchema, DocumentSchema
 
 router = Router()
+
+
+@router.get("/scopes", response=List[ScopeSchema], tags=["core"])
+def list_scopes(request):
+    qs = Scope.objects.all()
+    return qs
+
+
+@router.get("/scopes/{scope_id}", response=ScopeSchema, tags=["core"])
+def get_scope(request, topic_id: int):
+    item = get_object_or_404(Scope, id=scope_id)
+    return item
 
 
 @router.get("/topics", response=List[TopicSchema], tags=["core"])
@@ -19,13 +31,28 @@ def get_topic(request, topic_id: int):
     return item
 
 
+@router.get("/filters", response=FilterSchema, tags=["core"])
+def list_filters(request):
+    """Returns all the filters in a single query"""
+    scopes = list(Scope.objects.all())
+    topics = list(Topic.objects.all())
+
+    qs = {"scopes": scopes, "topics": topics}
+    return qs
+
+
 @router.get("/documents", response=List[DocumentSchema], tags=["core"])
-def list_documents(request):
+def list_documents(request, topic: int = None, scope: int = None, page: int = None):
     """
     Lists all **published** documents.
     """
     qs = Document.objects.filter(is_published=True)
-    print(qs)
+    if topic:
+        qs = qs.filter(topics__id=topic)
+    if scope:
+        qs = qs.filter(scope__id=scope)
+    if page:
+        qs = qs.filter(publication_pages__id=page)
     return qs
 
 
--- core/schemas.py
+++ core/schemas.py
@@ -1,11 +1,18 @@
 from ninja import Schema
 from typing import List
+from datetime import date
 
 
 class TopicSchema(Schema):
     id: int
     name: str
-    icon_url: str = None
+    icon_path: str = None
+
+
+class DocumentTypeSchema(Schema):
+    id: int
+    name: str
+    icon_path: str = None
 
 
 class PageTypeSchema(Schema):
@@ -18,16 +25,83 @@ class ScopeSchema(Schema):
     name: str
 
 
+class FilterSchema(Schema):
+    scopes: List[ScopeSchema]
+    topics: List[TopicSchema]
+
+
+class OrganizationSchema(Schema):
+    id: int
+    name: str
+    acronym: str
+    part_of: "OrganizationSchema" = None
+    logo: str = None
+
+
+OrganizationSchema.update_forward_refs()
+
+
+class SourceSchema(Schema):
+    id: int
+    title: str
+    last_update: date
+    editor: List[OrganizationSchema]
+    """
+    rss_feed = models.OneToOneField(
+        "feeds.Source", verbose_name="flux RSS", on_delete=models.SET_NULL, null=True
+    )
+    url = models.URLField("URL", null=True, blank=True)
+    base_domain = models.CharField("domaine", max_length=100, null=True, blank=True)
+    scope = models.ManyToManyField(Scope, verbose_name="portée", blank=True)
+    topics = models.ManyToManyField(Topic, verbose_name="sujet", blank=True)
+    years = models.ManyToManyField(DataYear, verbose_name="année", blank=True)
+    regions = models.ManyToManyField(
+        "francesubdivisions.Region", verbose_name="région", blank=True
+    )
+    departements = models.ManyToManyField(
+        "francesubdivisions.Departement",
+        verbose_name="département",
+        blank=True,
+    )
+    epcis = models.ManyToManyField(
+        "francesubdivisions.Epci",
+        verbose_name="EPCI",
+        blank=True,
+    )
+    communes = models.ManyToManyField(
+        "francesubdivisions.Commune",
+        verbose_name="commune",
+        blank=True,
+    )
+    document_type = models.ManyToManyField(
+        DocumentType,
+        verbose_name="type des documents inclus",
+        blank=True,
+        related_name="documents_type",
+    )
+    source_type = models.ManyToManyField(
+        DocumentType,
+        verbose_name="type de source",
+        blank=True,
+        related_name="source_type",
+    )
+    """
+
+
 class DocumentSchema(Schema):
+    id: int
     url: str
     title: str
     base_domain: str
     publication_pages: List[PageTypeSchema]
     scope: List[ScopeSchema]
+    topics: List[TopicSchema] = None
+    document_type: List[DocumentTypeSchema] = None
+    last_update: date
+    source: SourceSchema
+    body: str = None
 
     """
-    scope = models.ManyToManyField(Scope, verbose_name="portée", blank=True)
-    topics = models.ManyToManyField(Topic, verbose_name="sujet", blank=True)
     years = models.ManyToManyField(DataYear, blank=True)
     regions = models.ManyToManyField(
         "francesubdivisions.Region", verbose_name="région", blank=True
@@ -57,8 +131,4 @@ class DocumentSchema(Schema):
         null=True,
         blank=True,
     )
-    document_type = models.ManyToManyField(
-        DocumentType, verbose_name="type de document", blank=True
-    )
-    last_update = models.DateField("dernière mise à jour", null=True, blank=True)
     """
