8ea5c2afb33a6b09c6b0e3ab97c707b97a30d532
Ash_Crow <sylvain@ashtree.eu>
2021-04-12 10:12:35 +0200

    fix image path

--- core/views.py
+++ core/views.py
@@ -1,10 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse(
-        """
-        <h1>Bienvenue sur Open Collectivités!</h1>
-        <p>La documentation de l’API se trouve sur la page <a href="api/docs">/api/docs</a> 
-        """
-    )
+    return render(request, "core/index.html", {"title": "OpenCo"})
