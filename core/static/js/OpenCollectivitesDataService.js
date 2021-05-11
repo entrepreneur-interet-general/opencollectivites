ec00556444108f669d6cc997c4354b5a69ceb006
Ash_Crow <sylvain@ashtree.eu>
2021-05-11 16:52:23 +0200

    Add missing semicolons

--- core/static/js/OpenCollectivitesDataService.js
+++ core/static/js/OpenCollectivitesDataService.js
@@ -10,16 +10,15 @@ class OpenCollectivitesDataService {
     return apiCall.get(`/france/subdivisions/${name}`);
   }
   communeBySiren(siren) {
-    return apiCall.get(`/france/communes/siren/${siren}`)
+    return apiCall.get(`/france/communes/siren/${siren}`);
   }
   aspicFicheCommune(siren) {
-    return apiCall.get(`/aspic/fiche-commune/${siren}`)
+    return apiCall.get(`/aspic/fiche-commune/${siren}`);
   }
   publicationFilters() {
-    return apiCall.get(`/core/filters`)
+    return apiCall.get(`/core/filters`);
   }
   getPublications(filters) {
-    return apiCall.get(`/core/documents`, { params: filters })
+    return apiCall.get(`/core/documents`, { params: filters });
   }
-
 }
