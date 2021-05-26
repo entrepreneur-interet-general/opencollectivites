d38ccb1ffe22ad7d379c90286e42807f1fac636e
Ash_Crow <sylvain@ashtree.eu>
2021-05-26 11:03:23 +0200

    This file should now be tracked

---
+++ gunicorn_start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -a
+[ -f  .env] && . .env
+set +a
+
+echo "Starting $NAME as `whoami`"
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec $PIPENV run gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user $USER \
+  --bind=unix:$SOCKFILE
