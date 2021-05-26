de2b1b6dd76f2ee6c753aa977d0184cd02118414
Ash_Crow <sylvain@ashtree.eu>
2021-05-26 11:50:17 +0200

    [Devops] Moved deployment scripts and env to the devops directory

--- devops/gunicorn_start.sh
+++ devops/gunicorn_start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 set -a
-[ -f  .env ] && . .env
+[ -f  $SCRIPT_DIR/.env ] && . $SCRIPT_DIR/.env && echo "Local environment variables loaded"
 set +a
 
 echo "Starting $NAME as `whoami`"
 
 # Create the run directory if it doesn't exist
-RUNDIR=$(dirname $SOCKFILE)
-test -d $RUNDIR || mkdir -p $RUNDIR
+RUN_DIR=$(dirname $SOCKFILE)
+test -d $RUN_DIR || mkdir -p $RUN_DIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
---
+++ devops/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+set -a
+[ -f  $SCRIPT_DIR/.env ] && . $SCRIPT_DIR/.env && echo "Local environment variables loaded"
+set +a
+
+echo "Updating $NAME as `whoami`"
+
+sudo service $GUNICORN_SERVICE stop && echo "Service $GUNICORN_SERVICE stopped"
+
+# Go to the directory and update if needed
+cd $DJANGO_DIR
+git pull
+git submodule update
+$PIPENV install
+$PIPENV run python manage.py migrate
+$PIPENV run python manage.py collectstatic --no-input
+
+sudo service $GUNICORN_SERVICE start && echo "Service $GUNICORN_SERVICE started"
+sudo service $GUNICORN_SERVICE status
