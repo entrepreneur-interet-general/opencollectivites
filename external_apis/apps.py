b541632b40ec73262d85029dd56a25dd003a2dac
Ash_Crow <sylvain@ashtree.eu>
2021-11-10 15:39:31 +0100

    [Admin] Renamed app names for admin panels

--- external_apis/apps.py
+++ external_apis/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ExternalApisConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "external_apis"
-    verbose_name = "Externe"
+    verbose_name = "Requêtes externes"
