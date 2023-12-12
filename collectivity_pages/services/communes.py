e586670a22d42ecc9ce9b3ec6f46f50a5899bc54
jdesclodure <103088250+jdesclodure@users.noreply.github.com>
2023-12-12 09:17:22 +0100

    fall back to fork commit

--- collectivity_pages/services/communes.py
+++ collectivity_pages/services/communes.py
@@ -92,14 +92,6 @@ def commune_data(commune: Commune, year: DataYear = None):
     response["tables"] = context_data.formated_tables
     response["max_year"] = context_data.max_year
 
-    x_data = [0,1,2,3]
-    y_data = [x**2 for x in x_data]
-    plot_div = plot([Scatter(x=x_data, y=y_data,
-                        mode='lines', name='test',
-                        opacity=0.8, marker_color='green')],
-               output_type='div')
-    response["plot_div"] = plot_div
-
     return response
 
 
